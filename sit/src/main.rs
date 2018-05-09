9955823a8b27274f9a17ccc38eb236aba71e387b
Yurii Rashkovskii <me@yrashk.com>
2018-05-08 18:18:08 -0700

    Problem: `sit items` is not covered with tests
    Solution: provide initial coverage

--- sit/src/main.rs
+++ sit/src/main.rs
@@ -28,6 +28,7 @@ mod command_args;
 mod command_init;
 mod command_item;
 mod command_record;
+mod command_items;
 
 #[cfg(unix)]
 extern crate xdg;
@@ -42,7 +43,6 @@ extern crate glob;
 extern crate atty;
 
 extern crate rayon;
-use rayon::prelude::*;
 
 extern crate question;
 
@@ -51,7 +51,7 @@ extern crate dunce;
 extern crate which;
 
 use std::collections::HashMap;
-fn get_named_expression<S: AsRef<str>>(name: S, repo: &sit_core::Repository,
+pub fn get_named_expression<S: AsRef<str>>(name: S, repo: &sit_core::Repository,
                                        repo_path: S, exprs: &HashMap<String, String>) -> Option<String> {
     let path = repo.path().join(repo_path.as_ref()).join(name.as_ref());
     if path.is_file() {
@@ -378,52 +378,7 @@ fn main_with_result(allow_external_subcommands: bool) -> i32 {
         }
 
         if let Some(matches) = matches.subcommand_matches("items") {
-            let items: Vec<_> = repo.item_iter().expect("can't list items").collect();
-
-            let filter_expr = matches.value_of("named-filter")
-                .and_then(|name|
-                              get_named_expression(name, &repo, ".items/filters", &config.items.filters))
-                .or_else(|| matches.value_of("filter").or_else(|| Some("`true`")).map(String::from))
-                .unwrap();
-
-            let filter_defined = matches.is_present("named-filter") || matches.is_present("filter");
-
-            let query_expr = matches.value_of("named-query")
-                .and_then(|name|
-                              get_named_expression(name, &repo, ".items/queries", &config.items.queries))
-                .or_else(|| matches.value_of("query").or_else(|| Some("id")).map(String::from))
-                .unwrap();
-
-            let filter = jmespath::compile(&filter_expr).expect("can't compile filter expression");
-            let query = jmespath::compile(&query_expr).expect("can't compile query expression");
-
-            let mut reducer = sit_core::reducers::duktape::DuktapeReducer::new(&repo).unwrap();
-            let items_with_reducers: Vec<_> =  items.into_iter().map(|i| (i, reducer.clone())) .collect();
-            items_with_reducers.into_par_iter()
-                .map(|(item, mut reducer)| {
-                    let result = item.reduce_with_reducer(&mut reducer).expect("can't reduce item");
-                    let data = jmespath::Variable::from(serde_json::Value::Object(result));
-                    let result = if filter_defined {
-                        filter.search(&data).unwrap().as_boolean().unwrap()
-                    } else {
-                        true
-                    };
-                    if result {
-                        let view = query.search(&data).unwrap();
-                        if view.is_string() {
-                            Some(view.as_string().unwrap().clone())
-                        } else {
-                            Some(serde_json::to_string_pretty(&view).unwrap())
-                        }
-                    } else {
-                        None
-                    }
-                })
-                .filter(Option::is_some).map(Option::unwrap)
-                .for_each(|view| {
-                    println!("{}", view);
-                });
-                return 0;
+            return command_items::command(matches, &repo, config);
         }
 
         if let Some(matches) = matches.subcommand_matches("record") {
