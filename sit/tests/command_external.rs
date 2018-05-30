26a6d2a6ee5f262a94a5b09f69752f5bef65088e
Yurii Rashkovskii <me@yrashk.com>
2018-05-29 21:06:36 -0700

    Problem: calling external CLI commands is not tested
    Solution: provide basic test coverage

---
+++ sit/tests/command_external.rs
@@ -0,0 +1,188 @@
+extern crate cli_test_dir;
+
+use cli_test_dir::*;
+#[cfg(unix)]
+use std::fs;
+#[cfg(unix)]
+use std::os::unix::fs::PermissionsExt;
+
+#[allow(unused_variables)]
+fn create_script(dir: &TestDir, unix_path: &str, windows_path: &str, unix: &str, windows: &str) {
+    #[cfg(unix)] {
+        dir.create_file(unix_path, unix);
+        let mut perms = fs::metadata(dir.path(unix_path)).unwrap().permissions();
+        perms.set_mode(0o766);
+        fs::set_permissions(dir.path(unix_path), perms).unwrap();
+    }
+    #[cfg(windows)]
+    dir.create_file(windows_path, windows);
+}
+
+/// Should fail on an unavailable command
+#[test]
+fn ext_unavailable() {
+    let dir = TestDir::new("sit", "ext_unavailable");
+    dir.cmd()
+        .arg("init")
+        .expect_success();
+    dir.cmd()
+        .arg("does-not-exist")
+        .expect_failure();
+}
+
+/// Should pass SIT variable pointing to SIT itself
+#[test]
+fn ext_sit_path() {
+    let dir = TestDir::new("sit", "ext_cli");
+    dir.cmd()
+        .arg("init")
+        .expect_success();
+    create_script(&dir, ".sit/cli/sit-path", ".sit/cli/sit-path.bat", r#"#! /bin/bash
+        echo -n ${SIT}
+    "#, r#"
+    @echo off
+    echo %SIT%
+    "#);
+
+    let result = String::from_utf8(dir.cmd()
+        .arg("path")
+        .expect_success().stdout).unwrap();
+
+    dir.expect_path(result.trim());
+}
+
+/// Should execute an external command available in .sit/cli
+#[test]
+fn ext_cli() {
+    let dir = TestDir::new("sit", "ext_cli");
+    dir.cmd()
+        .arg("init")
+        .expect_success();
+
+    create_script(&dir, ".sit/cli/sit-exists", ".sit/cli/sit-exists.bat", r#"#! /bin/bash
+        echo SIT_DIR=${SIT_DIR}
+    "#, r#"
+    @echo off
+    echo SIT_DIR=%SIT_DIR%
+    "#);
+
+    let result = String::from_utf8(dir.cmd()
+        .arg("exists")
+        .expect_success().stdout).unwrap().replace("\r","");
+
+    assert_eq!(result, format!("SIT_DIR={}\n", dir.path(".sit").to_str().unwrap()))
+}
+
+/// Should execute an external command available in .sit/modules/*/cli
+#[test]
+fn ext_modules_cli() {
+    let dir = TestDir::new("sit", "ext_modules_cli");
+    dir.cmd()
+        .arg("init")
+        .expect_success();
+
+    create_script(&dir, ".sit/modules/test/cli/sit-exists", ".sit/modules/test/cli/sit-exists.bat", r#"#! /bin/bash
+        echo SIT_DIR=${SIT_DIR}
+    "#, r#"
+    @echo off
+    echo SIT_DIR=%SIT_DIR%
+    "#);
+
+    let result = String::from_utf8(dir.cmd()
+        .arg("exists")
+        .expect_success().stdout).unwrap().replace("\r","");
+
+    assert_eq!(result, format!("SIT_DIR={}\n", dir.path(".sit").to_str().unwrap()))
+}
+
+/// Should execute an external command available in PATH
+#[test]
+fn ext_modules_path() {
+    let dir = TestDir::new("sit", "ext_modules_path");
+    dir.cmd()
+        .arg("init")
+        .expect_success();
+
+    create_script(&dir, "sit-exists", "sit-exists.bat", r#"#! /bin/bash
+        echo SIT_DIR=${SIT_DIR}
+    "#, r#"
+    @echo off
+    echo SIT_DIR=%SIT_DIR%
+    "#);
+
+    let result = String::from_utf8(dir.cmd()
+        .env("PATH", dir.path(".").to_str().unwrap())
+        .arg("exists")
+        .expect_success().stdout).unwrap().replace("\r","");
+
+    assert_eq!(result, format!("SIT_DIR={}\n", dir.path(".sit").to_str().unwrap()))
+}
+
+/// Command in .sit/cli should take precedence over a command in .sit/modules/*/cli and PATH
+#[test]
+fn ext_cli_over_modules_cli_and_path() {
+    let dir = TestDir::new("sit", "ext_cli_over_modules_cli_and_path");
+    dir.cmd()
+        .arg("init")
+        .expect_success();
+
+    create_script(&dir, "sit-exists", "sit-exists.bat", r#"#! /bin/bash
+    echo path
+    "#, r#"
+    @echo off
+    echo path
+    "#);
+
+    create_script(&dir, ".sit/cli/sit-exists", ".sit/cli/sit-exists.bat", r#"#! /bin/bash
+    echo cli
+    "#, r#"
+    @echo off
+    echo cli
+    "#);
+
+    create_script(&dir, ".sit/modules/test/cli/sit-exists", ".sit/modules/test/cli/sit-exists.bat", r#"#! /bin/bash
+    echo modules
+    "#, r#"
+    @echo off
+    echo modules
+    "#);
+
+
+    let result = String::from_utf8(dir.cmd()
+        .env("PATH", dir.path(".").to_str().unwrap())
+        .arg("exists")
+        .expect_success().stdout).unwrap().replace("\r","");
+
+    assert_eq!(result, "cli\n")
+}
+
+/// Command in .sit/modules/cli should take precedence over a command in PATH
+#[test]
+fn ext_modules_over_path() {
+    let dir = TestDir::new("sit", "ext_modules_over_path");
+    dir.cmd()
+        .arg("init")
+        .expect_success();
+
+    create_script(&dir, "sit-exists", "sit-exists.bat", r#"#! /bin/bash
+    echo path
+    "#, r#"
+    @echo off
+    echo path
+    "#);
+
+    create_script(&dir, ".sit/modules/test/cli/sit-exists", ".sit/modules/test/cli/sit-exists.bat", r#"#! /bin/bash
+    echo modules
+    "#, r#"
+    @echo off
+    echo modules
+    "#);
+
+
+    let result = String::from_utf8(dir.cmd()
+        .env("PATH", dir.path(".").to_str().unwrap())
+        .arg("exists")
+        .expect_success().stdout).unwrap().replace("\r","");
+
+    assert_eq!(result, "modules\n")
+}
